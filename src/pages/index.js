fcd53907e7d382fd65b8bbe43946a8aae8af6613
zxch91 <lolget360ed@gmail.com>
2023-03-17 19:02:27 +0000

    Added visibility to password

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,20 @@
 import React, { useState } from 'react';
-import { Button, TextField, Typography } from '@mui/material';
+import { Button, TextField, Typography, IconButton} from '@mui/material';
 import styles from '@/styles/Login.module.css';
 import Image from 'next/image';
 import { useRouter } from "next/router";
-import { useAuth } from '@/hooks/useAuth';
+import { Visibility, VisibilityOff } from '@mui/icons-material';
 
 
 
 const Login = () => {
   const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [password, setPassword] = useState({password: '', showPassword: false});
   const router = useRouter();
+
+  const handleShowPassword = () => {
+    setPassword({...password, showPassword:!password.showPassword}); // creates ability to toggle password visibility
+  }
 
   const handleLogin = () => {
     // Handle login logic here
@@ -24,9 +28,17 @@ const Login = () => {
       <form onSubmit={handleLogin} className={styles.form}>
         <TextField label="Username" variant="outlined" value={username} onChange={(e) => setUsername(e.target.value)} className={styles.input} />
         <br />
-        <TextField label="Password" type="password" variant="outlined" value={password} onChange={(e) => setPassword(e.target.value)} className={styles.input} />
+        <TextField label="Password" type={password.showPassword ? 'text' : 'password'} variant="outlined" value={password.password} 
+onChange={(e) => setPassword({ ...password, password: e.target.value })} className={styles.input} InputProps={{
+    endAdornment: (
+      <IconButton onClick={handleShowPassword}>
+        {password.showPassword ? <VisibilityOff /> : <Visibility />}
+      </IconButton>
+    )
+  }}
+/>
         <br />
-        <Button variant="contained" onClick={handleLogin} className={styles.button}>Loginn</Button>
+        <Button variant="contained" onClick={handleLogin} className={styles.button}>Login</Button>
       </form>
     </div>
   );
