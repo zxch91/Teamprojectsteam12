34c6766a56fd353ae261f90db29025e7f1a8034d
Zach <108234212+zxch91@users.noreply.github.com>
2023-03-21 13:52:15 +0000

    fix override
    fix override

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,32 +1,82 @@
-import React, { useState } from 'react';
-import { Button, TextField, Typography } from '@mui/material';
-import styles from '@/styles/Login.module.css';
-import Image from 'next/image';
+import React, { useState } from "react";
+import { Button, TextField, Typography, IconButton } from "@mui/material";
+import styles from "@/styles/Login.module.css";
+import Image from "next/image";
 import { useRouter } from "next/router";
-import Header from '@/components/header';
-
-
+import { Visibility, VisibilityOff } from "@mui/icons-material";
 
 const Login = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState({
+    password: "",
+    showPassword: false,
+  });
   const router = useRouter();
 
+  const handleShowPassword = () => {
+    setPassword({ ...password, showPassword: !password.showPassword }); // creates ability to toggle password visibility
+  };
+
+  const url = "/api/users"; 
   const handleLogin = () => {
-    // Handle login logic here
-    console.log(`Username: ${username}, Password: ${password}`);
-    router.push('/landing');
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password: password.password }),
+    })
+      .then((response) => {
+        if (response.ok) {
+          // Login successful, redirect to landing page
+          router.push("/landing");
+        } else {
+          // Login failed, display error message
+          console.log("Login failed");
+        }
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
   };
 
   return (
     <div className={styles.container}>
       <Image src="/Logo.png" width={425} height={100} alt="logo" />
       <form onSubmit={handleLogin} className={styles.form}>
-        <TextField label="Username" variant="outlined" value={username} onChange={(e) => setUsername(e.target.value)} className={styles.input} />
+        <TextField
+          label="Username"
+          variant="outlined"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          className={styles.input}
+        />
         <br />
-        <TextField label="Password" type="password" variant="outlined" value={password} onChange={(e) => setPassword(e.target.value)} className={styles.input} />
+        <TextField
+          label="Password"
+          type={password.showPassword ? "text" : "password"}
+          variant="outlined"
+          value={password.password}
+          onChange={(e) =>
+            setPassword({ ...password, password: e.target.value })
+          }
+          className={styles.input}
+          InputProps={{
+            endAdornment: (
+              <IconButton onClick={handleShowPassword}>
+                {password.showPassword ? <VisibilityOff /> : <Visibility />}
+              </IconButton>
+            ),
+          }}
+        />
         <br />
-        <Button variant="contained" onClick={handleLogin} className={styles.button}>Login</Button>
+        <Button
+          variant="contained"
+          onClick={handleLogin}
+          className={styles.button}
+        >
+          Login
+        </Button>
       </form>
     </div>
   );
