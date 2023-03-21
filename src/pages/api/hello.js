1abfdb7377bae5d7ffff79f0052199ae60f67f08
Peter-Wild <115560867+Peter-Wild@users.noreply.github.com>
2023-03-21 13:41:03 +0000

    First step in sql connection

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,22 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+function createConnection() {
+  $servername = "127.0.0.1:3306";
+  $username = "remote";
+  $password = "make-it-all";
+  $dbname = "store";
+
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
+
+  // Check connection
+  //if ($conn->connect_error) {
+  //  die("Connection failed: " . $conn->connect_error);
+  //}
+
+  return $conn;
+}
+
 export default function handler(req, res) {
   res.status(200).json({ name: 'John Doe' })
 }
