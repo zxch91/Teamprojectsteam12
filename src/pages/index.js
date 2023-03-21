68f2896e3857d15355c5184cea7a655a40be7bce
ollyperry02 <118985155+ollyperry02@users.noreply.github.com>
2023-03-21 13:30:49 +0000

    Creation of header

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,9 +20,6 @@ const Login = () => {
 
   return (
     <div className={styles.container}>
-    <div>
-      <Header />
-    </div>
       <Image src="/Logo.png" width={425} height={100} alt="logo" />
       <form onSubmit={handleLogin} className={styles.form}>
         <TextField label="Username" variant="outlined" value={username} onChange={(e) => setUsername(e.target.value)} className={styles.input} />
