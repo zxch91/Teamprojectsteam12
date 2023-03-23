a3b143f750c53509881987e3e5141546f2f36057
ollyperry02 <118985155+ollyperry02@users.noreply.github.com>
2023-03-23 12:25:37 +0000

    Add files via upload

---
+++ header.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Router from "next/router";
+import styles from '@/styles/Header.module.css';
+import Image from 'next/image';
+import { Button, TextField, Typographys } from '@mui/material';
+
+const Header = () => {
+
+
+     return (
+      <div className = {styles.headerContainer}>
+        <div className = {styles.headerElements}>
+          <div className = {styles.logoContainer}>
+            <Image src="/Logo.png" width={160} height={35} alt="logo" />
+          </div>   
+        </div>
+        <div className = {styles.loggedInContainer}>
+          
+          <Button>Someone</Button>
+        </div>
+      </div>
+      
+    );
+  };
+  
+  export default Header;    
