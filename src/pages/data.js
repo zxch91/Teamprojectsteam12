3c7aec708ccf73a5b08d941fbc926eb7605c7732
WJClarke <91890060+WJClarke@users.noreply.github.com>
2023-04-27 13:37:04 +0100

    updated data.js added user table tag

--- src/pages/data.js
+++ src/pages/data.js
@@ -1,2 +1,23 @@
 import React from 'react';
 import Chart from 'chart.js/auto';
+import styles from '@/styles/Data.module.css';
+import UserTable from '@/components/UserTable';
+
+
+export default function DataAnalytics() {
+    
+    
+    //Leaders view (teams they are in)
+
+
+    //Managers View (all teams on system)
+
+    return (
+        <div>
+            <div class='center'><h1>All Users</h1></div>
+            <UserTable />
+        </div>
+    );
+
+};
+
