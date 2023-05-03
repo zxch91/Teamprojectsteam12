1faee88d635cfce6453c5a5a6bcf9480b644df5b
zxch91 <lolget360ed@gmail.com>
2023-05-03 15:48:05 +0100

    Now sends newly input messages to the server

--- src/pages/chat.js
+++ src/pages/chat.js
@@ -1,96 +1,103 @@
-import { useState, useEffect } from 'react';
-import MessageList from '../components/messageList';
-import { Container, Box, Typography, TextField } from '@mui/material';
-import styles from '@/styles/Chat.module.css';
-import Sidebar from '../components/Sidebar';
-import Channel from '../components/Channel';
-import ChatBox from '../components/ChatBox';
+import { useState, useEffect } from "react";
+import MessageList from "../components/messageList";
+import { Container, Box, Typography, TextField } from "@mui/material";
+import styles from "@/styles/Chat.module.css";
+import Sidebar from "../components/Sidebar";
+import Channel from "../components/Channel";
+import ChatBox from "../components/ChatBox";
 
 export default function Chat() {
   const [messages, setMessages] = useState([]);
-  const [inputMessage, setInputMessage] = useState('');
+  const [inputMessage, setInputMessage] = useState("");
   const [selectedChannel, setSelectedChannel] = useState(false);
   const [channel, setChannels] = useState([]);
   const [search, setSearch] = useState("");
 
   useEffect(() => {
-    fetch('/api/message')
+    fetch("/api/message")
       .then((response) => response.json())
       .then((data) => setMessages(data));
   }, []);
 
   const channels = [
-    { id: 1, title: 'Channel 1' },
-    { id: 2, title: 'Channel 2' },
-    { id: 3, title: 'Channel 3' },
+    { id: 1, title: "Channel 1" },
+    { id: 2, title: "Channel 2" },
+    { id: 3, title: "Channel 3" },
   ];
 
   var requestOptions = {
-    method: 'GET',
-    redirect: 'follow'
+    method: "GET",
+    redirect: "follow",
   };
 
   const fetchChannels = () => {
     return fetch("api/viewchat", requestOptions)
-      .then(response => response.json())
-      .then(result => result.result) // extract channels from result property
-      .catch(error => console.log('error', error));
-  }
-  
+      .then((response) => response.json())
+      .then((result) => result.result) // extract channels from result property
+      .catch((error) => console.log("error", error));
+  };
 
   useEffect(() => {
     fetchChannels().then((res) => {
       if (res) {
-        const channels = []
+        const channels = [];
         for (const c in res) {
           const channel = {
-            id : res[c].group_id,
-            name: res[c].group_name
-          }
+            id: res[c].group_id,
+            name: res[c].group_name,
+          };
           channels.push(channel);
         }
-        setChannels(channels)
+        setChannels(channels);
       }
-    })
+    });
   }, []);
-  
 
   useEffect(() => {
     console.log(channel);
   }, [channel]);
 
-
   const sendMessage = async (e) => {
-    e.preventDefault();
-    if (inputMessage.trim() !== '') {
-      await fetch('/api/messages', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ message: { sender: 'testuser', text: inputMessage } }),
-      });
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
 
-      setInputMessage('');
-      fetch('/api/messages')
-        .then((response) => response.json())
-        .then((data) => setMessages(data));
-    }
+    var urlencoded = new URLSearchParams();
+    urlencoded.append("senderId", "1");
+    urlencoded.append("recipientId", selectedChannel.id);
+    urlencoded.append("content", inputMessage);
+
+    var requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: urlencoded,
+      redirect: "follow",
+    };
+
+    fetch("http://localhost:3000/api/message", requestOptions)
+      .then((response) => response.text())
+      .then((result) => console.log(result))
+      .catch((error) => console.log("error", error));
   };
 
   const handleChannelSelect = (channel) => {
     setSelectedChannel(channel);
-    fetch(`/api/messages?channel=${channel.id}`)
+    fetch(`/api/message?channel=${channel.id}`)
       .then((response) => response.json())
       .then((data) => setMessages(data));
-      console.log(channel.id);
+    const chat = channel.id;
+    console.log(channel.id);
   };
 
   return (
-    <Container maxWidth="lg" sx={{ display: 'flex' }}>
+    <Container maxWidth="lg" sx={{ display: "flex" }}>
       <Sidebar className={styles.sidebar} />
       <Box flexGrow={0} width={200}>
-        <TextField id="chatSearch" label="Chat Search" variant="outlined" sx={{mt: "10px", mb:"5px"}} />
+        <TextField
+          id="chatSearch"
+          label="Chat Search"
+          variant="outlined"
+          sx={{ mt: "10px", mb: "5px" }}
+        />
         {channel.map((channel) => (
           <Channel
             key={channel.id}
@@ -106,6 +113,8 @@ export default function Chat() {
             inputMessage={inputMessage}
             setInputMessage={setInputMessage}
             sendMessage={sendMessage}
+            currentUser={"1"}
+            currentChat={selectedChannel.id}
           />
         )}
       </Box>
