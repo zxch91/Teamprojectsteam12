0a10ac767b5b1596b3211e9927351818a550a3c5
ollyperry02 <118985155+ollyperry02@users.noreply.github.com>
2023-05-04 18:11:46 +0100

    New Header
    I abandonded the center header idea because it looked terrible. I am going to resume my mission to try and prevent the sidebar from overlaping the header

--- src/components/header.js
+++ src/components/header.js
@@ -13,17 +13,19 @@ const Header = () => {
       router.push("/index");
     }
      return (
-      <div className = {styles.headerContainer}>
-        <div className = {styles.headerElements}>
-          <div className = {styles.logoContainer}>
-            <a onClick={HomePressed}>
-              <Image src="/Logo.png" width={160} height={35} alt="logo" />
-            </a>
-          </div>   
-        </div>
-        <div className = {styles.loggedInContainer}>
-          
-          <Button>Someone</Button>
+      <div className={styles.headerWrapper}>
+        <div className = {styles.headerContainer}>
+          <div className = {styles.headerElements}>
+            <div className = {styles.logoContainer}>
+              <a onClick={HomePressed}>
+                <Image src="/Logo.png" width={160} height={35} alt="logo" />
+              </a>
+            </div>   
+          </div>
+          <div className = {styles.loggedInContainer}>
+            
+            <Button>Someone</Button>
+          </div>
         </div>
       </div>
       
