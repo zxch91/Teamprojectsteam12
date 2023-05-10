4eb5811cbd9acc465c36113df18e63c086a55632
gedean01 <75091836+gedean01@users.noreply.github.com>
2023-05-10 15:18:26 +0100

    Add files via upload

--- src/pages/api/grabCurrentUserStats.js
+++ src/pages/api/grabCurrentUserStats.js
@@ -6,11 +6,12 @@ export default async function handler(req, res) {
     case "GET":
         //* GET to retrieve a resource
         //replace "###" with SESSION
-        let assignedTo = parseInt("4");
+        let assignedTo = parseInt(req.query["user_id"]);
         let isCompleted = parseInt(req.query["comp"]);
 
         let sqlGet = "select count(task_id) as 'num' from `Tasks` where assigned_to = ? and is_completed = ?;";
         sqlGet = mysql.format(sqlGet, [assignedTo, isCompleted]);
+        console.log(sqlGet);
         const resultGet = await executeQuery(sqlGet)
         res.status(200).json({ result: resultGet })
 
