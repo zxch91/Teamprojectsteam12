0e0ee17271feda18a26557f7a442309022702556
Peter-Wild <115560867+Peter-Wild@users.noreply.github.com>
2023-05-14 13:04:34 +0100

    Connecting locally on the database?

--- src/pages/api/db.js
+++ src/pages/api/db.js
@@ -10,69 +10,66 @@ const tunnelConfig = {
   host: "34.70.95.233",
   port: 22,
   username: "p_d_wild_21",
-  privateKey:  require('fs').readFileSync('reactServer')
+  privateKey: require('fs').readFileSync('reactServer')
 }
-//src\pages\api\db.js
-//reactServer
+
 const forwardConfig = {
-  srcHost: '127.0.0.1', // any valid address
-  srcPort: 3306, // any valid port
-  dstHost: dbServer.host, // destination database
-  dstPort: dbServer.port // destination port
+  srcHost: '127.0.0.1',
+  srcPort: 3306,
+  dstHost: dbServer.host,
+  dstPort: dbServer.port
 };
 
-// dbConfig.js
 const mysql = require('mysql2');
-const { Client } = require('ssh2');
-// create an instance of SSH Client
-const sshClient = new Client();
-
-const SSHConnection = new Promise((resolve, reject) => {
-
-  sshClient.on('ready', () => {
-    sshClient.forwardOut(
-    forwardConfig.srcHost,
-    forwardConfig.srcPort,
-    forwardConfig.dstHost,
-    forwardConfig.dstPort,
-    (err, stream) => {
-      if (err) {
-        reject(err);}
-
-            // create a new DB server object including stream
-            const updatedDbServer = {
-                 ...dbServer,
-                 stream
-            };
-            
-            // connect to mysql
-            const connection =  mysql.createConnection(updatedDbServer);
-            // check for successful connection
-            //  resolve or reject the Promise accordingly          
-            connection.connect((error) => {
-              if (error) {
-                reject(error);
-              }
-              resolve(connection);
-            });
-          });
-  }).connect(tunnelConfig);
-});
 
 async function executeQuery(sql) {
   try {
-    const connection = await SSHConnection;
+    let connection;
+    if (dbServer.host === 'localhost' || dbServer.host === '127.0.0.1') {
+      connection = mysql.createConnection(dbServer);
+    } else {
+      const { Client } = require('ssh2');
+      const sshClient = new Client();
+      const SSHConnection = new Promise((resolve, reject) => {
+        sshClient.on('ready', () => {
+          sshClient.forwardOut(
+            forwardConfig.srcHost,
+            forwardConfig.srcPort,
+            forwardConfig.dstHost,
+            forwardConfig.dstPort,
+            (err, stream) => {
+              if (err) {
+                reject(err);
+              }
+              const updatedDbServer = {
+                ...dbServer,
+                stream
+              };
+              const connection = mysql.createConnection(updatedDbServer);
+              connection.connect((error) => {
+                if (error) {
+                  reject(error);
+                }
+                resolve(connection);
+              });
+            });
+        }).connect(tunnelConfig);
+      });
+      connection = await SSHConnection;
+    }
     const result = await new Promise((resolve, reject) => {
       connection.query(sql, function (err, result) {
-        if (err) reject(err);
+        if (err) {
+          reject(err);
+        }
         resolve(result);
       });
     });
+    connection.end();
     return result;
   } catch (error) {
     return error;
   }
 }
-
 
 export default executeQuery;
